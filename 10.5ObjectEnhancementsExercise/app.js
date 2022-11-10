d7452a202627b192986e801adb019d1512109afc
Danielewin8 <danielewin8@gmail.com>
2022-11-09 17:06:50 -0800

    Finsiehd Object Enhancements Exercise

---
+++ 10.5ObjectEnhancementsExercise/app.js
@@ -0,0 +1,41 @@
+// Same Keys and Values 
+
+function createInstructor(firstName, lastName){
+    return{
+        firstName,
+        lastName
+    }
+}
+
+// Computed Property Names 
+
+let favoriteNumber = 42;
+
+// const instructor = {
+//     firstName: "Colt",
+//     [favoriteNumber]: "That is my favorite!"
+// }
+
+// Object Methods 
+
+const instructor = {
+    firstName: "Colt",
+    sayHi() {
+        return "Hi!"
+    },
+    sayBye() {
+        return this.firstName + " says bye!"
+    }
+}
+
+// createAnimal function 
+
+function createAnimal(animal, verb, noise){
+    return {
+        animal,
+        [verb](){
+            return noise;
+        }
+    }
+}
+
