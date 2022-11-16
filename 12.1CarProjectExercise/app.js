c57110a640942d366b5c3b11d1ea91866f704344
Danielewin8 <danielewin8@gmail.com>
2022-11-15 20:22:42 -0800

    Finished 12.1 Car Exercise

--- 12.1CarProjectExercise/app.js
+++ 12.1CarProjectExercise/app.js
@@ -11,7 +11,7 @@ class Vehicle {
     }
     toString(){
         return (`The vehicle is a ${this.make} ${this.model} from ${this.year} `);
-    }    
+    };   
 }
 
 // Makes a class of Car which inherits from Vehicle. Adds a property of numWheels to each instance 
@@ -20,7 +20,7 @@ class Car extends Vehicle{
     constructor(make, model, year){
         super(make, model, year);
         this.numWheels = 4;
-    }
+    };
 }
 
 // Makes a class called Motorcycle which also inherits from Vehicle, but this time numWheels has a value of 2. An extra method called revEngine is added. 
@@ -32,7 +32,7 @@ class Motorcycle extends Vehicle{
     }
     revEngine(){
         return "VROOM!!!";
-    }
+    };
 }
 
 // Makes a class called Garage, which will store an array and has a property called capacity. Added a method to add a vehicle, if the vehicle is not an instance of the previous class Vehicle, it is not a vehicle. If the capacity property is less than the length of the array of vehicles, the Garage is full. Otherwise a newVehicle can be added.
@@ -51,5 +51,5 @@ class Garage{
         }
         this.vehicles.push(newVehicle);
             return "Vehicle added!";
-    }
+    };
 }
