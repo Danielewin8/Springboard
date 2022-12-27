310634d8e79c8fa728b2295c3e34a99b65295ecd
Danielewin8 <danielewin8@gmail.com>
2022-12-27 14:03:13 -0800

    Finished 19.3 Exercise

--- 19.3/basic/app.py
+++ 19.3/basic/app.py
@@ -1,8 +1,8 @@
-from flask import Flask, request, render_template, redirect, flash, session
+from flask import Flask, request, render_template, redirect, flash
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey as survey
 
-RESPONSES_KEY = "responses"
+responses = []
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "never-tell!"
@@ -16,13 +16,6 @@ def start_page():
     
     return render_template("start-page.html", survey=survey)
 
-@app.route("/begin", methods=["POST"])
-def start_survey():
-
-    session[RESPONSES_KEY] = []
-
-    return redirect("/questions/0")
-
 @app.route("/answer", methods=["POST"])
 def handle_question():
     """Save response and redirect to next question."""
@@ -30,11 +23,10 @@ def handle_question():
     # get the response choice
     choice = request.form['answer']
 
-    # add this response to the session
-    responses = session[RESPONSES_KEY]
     responses.append(choice)
-    session[RESPONSES_KEY] = responses
 
+    # add this response to the session
+   
     if (len(responses) == len(survey.questions)):
         # They've answered all the questions! Thank them.
         return redirect("/complete")
@@ -46,7 +38,6 @@ def handle_question():
 @app.route("/questions/<int:qid>")
 def show_question(qid):
     """Display current question."""
-    responses = session.get(RESPONSES_KEY)
 
     if (responses is None):
         # trying to access question page too soon
