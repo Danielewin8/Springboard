f3f40f017239a631e4f5732e4f710b7802a0efc8
Danielewin8 <danielewin8@gmail.com>
2023-01-17 11:28:16 -0800

    Finished 22.4 SQL Exercise

---
+++ sql-ddl-design/craigslist.sql
@@ -0,0 +1,69 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+-- Modify this code to update the DB schema diagram.
+-- To reset the sample schema, replace everything with
+-- two dots ('..' - without quotes).
+
+CREATE TABLE "region" (
+    "ID" int   NOT NULL,
+    "name" string   NOT NULL,
+    CONSTRAINT "pk_region" PRIMARY KEY (
+        "ID"
+     ),
+    CONSTRAINT "uc_region_name" UNIQUE (
+        "name"
+    )
+);
+
+CREATE TABLE "users" (
+    "ID" int   NOT NULL,
+    "username" string   NOT NULL,
+    "password" string   NOT NULL,
+    CONSTRAINT "pk_users" PRIMARY KEY (
+        "ID"
+     ),
+    CONSTRAINT "uc_users_username" UNIQUE (
+        "username"
+    ),
+    CONSTRAINT "uc_users_password" UNIQUE (
+        "password"
+    )
+);
+
+CREATE TABLE "posts" (
+    "ID" int   NOT NULL,
+    "title" string   NOT NULL,
+    "text" string   NOT NULL,
+    "location" string   NOT NULL,
+    "users_id" int   NOT NULL,
+    "region_id" int   NOT NULL,
+    "category_id" int   NOT NULL,
+    CONSTRAINT "pk_posts" PRIMARY KEY (
+        "ID"
+     ),
+    CONSTRAINT "uc_posts_location" UNIQUE (
+        "location"
+    )
+);
+
+CREATE TABLE "category" (
+    "ID" int   NOT NULL,
+    "name" string   NOT NULL,
+    CONSTRAINT "pk_category" PRIMARY KEY (
+        "ID"
+     ),
+    CONSTRAINT "uc_category_name" UNIQUE (
+        "name"
+    )
+);
+
+ALTER TABLE "posts" ADD CONSTRAINT "fk_posts_users_id" FOREIGN KEY("users_id")
+REFERENCES "users" ("ID");
+
+ALTER TABLE "posts" ADD CONSTRAINT "fk_posts_region_id" FOREIGN KEY("region_id")
+REFERENCES "region" ("ID");
+
+ALTER TABLE "posts" ADD CONSTRAINT "fk_posts_category_id" FOREIGN KEY("category_id")
+REFERENCES "category" ("ID");
+
---
+++ sql-ddl-design/medical_center.sql
@@ -0,0 +1,69 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+-- Modify this code to update the DB schema diagram.
+-- To reset the sample schema, replace everything with
+-- two dots ('..' - without quotes).
+
+CREATE TABLE "doctor" (
+    "ID" int   NOT NULL,
+    "name" string   NOT NULL,
+    "profession" string   NOT NULL,
+    CONSTRAINT "pk_doctor" PRIMARY KEY (
+        "ID"
+     ),
+    CONSTRAINT "uc_doctor_profession" UNIQUE (
+        "profession"
+    )
+);
+
+CREATE TABLE "patients" (
+    "ID" int   NOT NULL,
+    "name" string   NOT NULL,
+    CONSTRAINT "pk_patients" PRIMARY KEY (
+        "ID"
+     )
+);
+
+CREATE TABLE "visits" (
+    "ID" int   NOT NULL,
+    "doctor_id" int   NOT NULL,
+    "patient_id" int   NOT NULL,
+    "date" int   NOT NULL,
+    CONSTRAINT "pk_visits" PRIMARY KEY (
+        "ID"
+     )
+);
+
+CREATE TABLE "diseases" (
+    "ID" int   NOT NULL,
+    "name" string   NOT NULL,
+    CONSTRAINT "pk_diseases" PRIMARY KEY (
+        "ID"
+     ),
+    CONSTRAINT "uc_diseases_name" UNIQUE (
+        "name"
+    )
+);
+
+CREATE TABLE "diagnoses" (
+    "ID" string   NOT NULL,
+    "visit_id" int   NOT NULL,
+    "diseases_id" int   NOT NULL,
+    CONSTRAINT "pk_diagnoses" PRIMARY KEY (
+        "ID"
+     )
+);
+
+ALTER TABLE "doctor" ADD CONSTRAINT "fk_doctor_ID" FOREIGN KEY("ID")
+REFERENCES "visits" ("doctor_id");
+
+ALTER TABLE "patients" ADD CONSTRAINT "fk_patients_ID" FOREIGN KEY("ID")
+REFERENCES "visits" ("patient_id");
+
+ALTER TABLE "visits" ADD CONSTRAINT "fk_visits_ID" FOREIGN KEY("ID")
+REFERENCES "diagnoses" ("visit_id");
+
+ALTER TABLE "diseases" ADD CONSTRAINT "fk_diseases_ID" FOREIGN KEY("ID")
+REFERENCES "diagnoses" ("diseases_id");
+
