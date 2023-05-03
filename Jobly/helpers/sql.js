f746510bcd6046df04a168dc62e90791391c61e7
Danielewin8 <danielewin8@gmail.com>
2023-05-03 13:56:18 -0700

    commit test

---
+++ Jobly/helpers/sql.js
@@ -0,0 +1,20 @@
+const { BadRequestError } = require("../expressError");
+
+// Helper function used in the Company model for updating data, transforms data into a SQL format, and sanitizes the data keys. 
+
+function sqlForPartialUpdate(dataToUpdate, jsToSql) {
+  const keys = Object.keys(dataToUpdate);
+  if (keys.length === 0) throw new BadRequestError("No data");
+
+  // {firstName: 'Aliya', age: 32} => ['"first_name"=$1', '"age"=$2']
+  const cols = keys.map((colName, idx) =>
+      `"${jsToSql[colName] || colName}"=$${idx + 1}`,
+  );
+
+  return {
+    setCols: cols.join(", "),
+    values: Object.values(dataToUpdate),
+  };
+}
+
+module.exports = { sqlForPartialUpdate };
